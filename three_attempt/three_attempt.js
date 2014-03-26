acc839b536c1a3ac552116ce470398ff2876f218
Cameron Erdogan <cameron.the.erdogan@gmail.com>
2014-03-25 23:07:51 -0400

    changed a lot of stuff, now using three.js

---
+++ three_attempt/three_attempt.js
@@ -0,0 +1,17 @@
+var geometry = new THREE.CubeGeometry(1,1,1);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 6;
+
+var render = function () {
+	requestAnimationFrame(render);
+
+	cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+};
+
+render();
