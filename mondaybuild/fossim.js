efb1cfe479d029ac2192afaca9d4a3fa0f5f1818
Cameron Erdogan <cameron.the.erdogan@gmail.com>
2014-04-14 18:53:12 -0400

    pretty much have general fosssim structure

--- mondaybuild/fossim.js
+++ mondaybuild/fossim.js
@@ -1,9 +1,30 @@
-var FOSSSim = {};
+// all THREE.js objects right now
 
-FOSSSim.Scene = function()
+var FOSSSim = {};
+var three_scene;
+var three_camera;
+var three_renderer;
+
+// basically main is just gonna be the outermost layer of the script
+
+
+function init_scene(canvas)
+{
+	
+}
+
+FOSSSim.Scene = function(canvas)
 {
 	// this.canvas = canvas;
-	this.paused = false;
+	var canvas_height = 500;
+    var canvas_width = 500;
+    rot_speed = 0.01;
+    three_scene = new THREE.Scene();
+    three_camera = new THREE.PerspectiveCamera(75, canvas_width/canvas_height, 0.1, 1000);
+    three_camera.lookAt(three_scene.position);	
+    // var canvas = document.getElementById("canvas");
+    three_renderer = new THREE.WebGLRenderer({"canvas": canvas});
+    three_renderer.setSize(canvas_width, canvas_height);
 };
 
 FOSSSim.Scene.prototype =
@@ -12,36 +33,7 @@ FOSSSim.Scene.prototype =
 
 	init: function()
 	{
-		// console.log(this.paused);
-
-		// var canvas = this.canvas;
-
-		// var canvas_height = 500;
-	 //    var canvas_width = 500;
-	 //    rot_speed = 0.01;
-	 //    var scene = new THREE.Scene();
-	 //    var camera = new THREE.PerspectiveCamera(75, canvas_width/canvas_height, 0.1, 1000);
-	 //    camera.lookAt(scene.position);	
-	 //    // var canvas = document.getElementById("canvas");
-	 //    var renderer = new THREE.WebGLRenderer({"canvas": canvas});
-	 //    renderer.setSize(canvas_width, canvas_height);
-
-	    // init_spheres();
-	    var num_spheres = 1;
-	    var geometry = new THREE.SphereGeometry( 1, 32, 16 );
-		var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-		sphere = new THREE.Mesh( geometry, material );
-		sphere.position.set(0,0,0);
-		scene.add(sphere);
-
-		// init_lights();
-		var light = new THREE.PointLight(0xffffff);
-		light.position.set(10,10,10);
-		scene.add(light);
-		var ambientlight = new THREE.AmbientLight( 0x404040 ); // soft white light
-		scene.add( ambientlight );
-
-		camera.position.z = 5;
+		three_camera.position.z = 5;
 		var paused = false;
 
 		// console.log(this.paused);
@@ -55,11 +47,30 @@ FOSSSim.Scene.prototype =
 				sphere.position.x += 0.01;
 				sphere.position.z += 0.01;
 
-				renderer.render(scene, camera);
+				three_renderer.render(three_scene, three_camera);
 			}
 		};
 		render();
 	}, 
+
+	init_spheres: function()
+	{
+		var num_spheres = 1;
+	    var geometry = new THREE.SphereGeometry( 1, 32, 16 );
+		var material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+		sphere = new THREE.Mesh( geometry, material );
+		sphere.position.set(0,0,0);
+		three_scene.add(sphere);
+	},
+
+	init_lights: function()
+	{
+		var light = new THREE.PointLight(0xffffff);
+		light.position.set(10,10,10);
+		three_scene.add(light);
+		var ambientlight = new THREE.AmbientLight( 0x404040 ); // soft white light
+		three_scene.add( ambientlight );
+	},
 
 	toggle_pause: function()
 	{
